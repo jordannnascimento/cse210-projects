9949901cf127e0da4a57fdf5c8ccd51b7930756e
jordannnascimento <jsantos7@byupathway.edu>
2026-05-08 20:48:17 -0300

    Week 01 exercises completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        // 1. Soma
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum = sum + n;
+        }
+
+        // 2. Média
+        double average = (double)sum / numbers.Count;
+
+        // 3. Maior número
+        int largest = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n > largest)
+            {
+                largest = n;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
